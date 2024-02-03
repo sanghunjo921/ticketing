79e981e9efdca3baec6244ccff44c0aebfee53b7
sanghunjo921 <salr921@gmail.com>
2024-02-04 02:27:53 +0900

    applied pagination

---
+++ src/locustTest/loadTest.py
@@ -0,0 +1,10 @@
+from locust import HttpUser, task, between
+
+class MyUser(HttpUser):
+    wait_time = between(0.5, 2)
+    load_balancer_dns = "ticketing-loadbalancer-1067889959.ap-northeast-2.elb.amazonaws.com"
+
+    @task
+    def get_tickets(self):
+        url = f"http://{self.load_balancer_dns}/tickets"
+        self.client.get(url)
