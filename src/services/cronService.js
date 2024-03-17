6078d68d181a05d7bc8e9fa45ef99e9882bef6c6
sanghunjo921 <salr921@gmail.com>
2024-03-17 23:39:37 +0900

    applied cron job

--- src/services/cronService.js
+++ src/services/cronService.js
@@ -1,32 +1,38 @@
+import { redisService } from "./RedisService";
+import cron from "node-cron";
+
 const { Transaction } = require("../models/Transaction");
 
-const batchSize = 3;
-const batchInterval = 5000;
+const batchInterval = "0 15 * * *";
 
 // Purchase 요청 처리 함수
-export const processBatchedRequests = async (batchedRequests) => {
+export const processBatchedRequests = async (batchedRequests, batchSize) => {
   if (batchedRequests.length === 0) return;
 
   console.log({ batchedRequests });
   console.log(`Batched requests count: ${batchedRequests.length}`);
 
   try {
-    if (batchedRequests.length > batchSize) {
-      const transactionsToCreate = batchedRequests.slice(0, batchSize); // batchSize만큼의 요청만 처리
-      await Transaction.bulkCreate(
-        transactionsToCreate.map((request) => ({
-          userId: request.userId,
-          ticketId: request.ticketId,
-          couponId: request.couponId,
-          totalPrice: request.totalPrice,
-        }))
-      );
-      batchedRequests.splice(0, batchSize + 1);
-      console.log("Batch processing completed.");
-    }
-    console.log({ batchedRequests });
-    console.log(`Batched requests count: ${batchedRequests.length}`);
+    const transactionsToCreate = batchedRequests.splice(0, batchSize);
+    await Transaction.bulkCreate(transactionsToCreate);
+    await redisService.setValue("transaction", batchedRequests);
+    console.log("Batch processing completed.");
+
+    console.log(`after Batched requests count: ${batchedRequests.length}`);
   } catch (error) {
     console.error("Error processing batched requests:", error);
   }
+};
+
+export const initiateCron = async () => {
+  cron.schedule(batchInterval, async () => {
+    try {
+      console.log("started cron");
+      const transactionDataKey = "transaction";
+      const batchedRequests = await redisService.getValue(transactionDataKey);
+      await processBatchedRequests(batchedRequests, batchedRequests.length);
+    } catch (error) {
+      next(error);
+    }
+  });
 };
